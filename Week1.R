a23070fae710c4c0893aeda28838fbd25ddd7625
KennyYahir <kenny.puma1030@gmail.com>
2016-11-29 17:03:46 -0600

    Some experiments about Time Series

--- Week1.R
+++ Week1.R
@@ -2,8 +2,43 @@ library(devtools)
 install_github("genomicsclass/GSE5859Subset")
 
 library(GSE5859Subset)
+
 data(GSE5859Subset) ##this loads the three tables
 
 head(sampleInfo)
 
+## Exercise 1 - - Week 1
+
 sum(sampleInfo$date == as.Date("2005-06-27"))
+
+## Excercise 2
+
+head(geneAnnotation)
+head(geneExpression)
+
+
+## Time series
+
+data("AirPassengers")
+class(AirPassengers)
+
+start(AirPassengers)
+cycle(AirPassengers)
+frequency(AirPassengers)
+
+plot(AirPassengers)
+
+plot(aggregate(AirPassengers, FUN = mean))
+ag <- aggregate(AirPassengers, FUN = mean)
+ag
+
+
+boxplot(AirPassengers~cycle(AirPassengers))
+air.dfs <- diff(AirPassengers)
+plot(diff(air.dfs))
+
+
+acf(AirPassengers)
+acf(diff(diff(AirPassengers)))  # better
+pacf((AirPassengers))
+

70fe9f07f6f0545185d00b679ffc6065bc62080d
KennyYahir <kenny.puma1030@gmail.com>
2016-12-01 17:18:00 -0600

    AdventOFCode begining

--- Week1.R
+++ Week1.R
@@ -42,3 +42,30 @@ acf(AirPassengers)
 acf(diff(diff(AirPassengers)))  # better
 pacf((AirPassengers))
 
+
+dat <- read.table("input1_a.in", header = F, sep = ",", stringsAsFactors = F, strip.white = T)
+lets <- sapply(1:ncol(dat), function(i) strsplit((dat[, i]), split = "")[[1]][1])
+ints <- sapply(1:ncol(dat), function(i) strsplit((dat[, i]), split = "")[[1]][2])
+
+lets <- strsplit((dat[1, 2]), split = "")[[1]][1]
+class(dat$V1)
+
+
+df <- data.frame("x" = lets, "y" = ints)
+write.table(df, file = "entrada1_a.in", sep = " ", col.names = F, row.names = F)
+
+ints <- as.numeric(ints)
+
+tot.sum <- sum(ints[1:2])
+
+myf <- function(j, arr, vals){
+  
+  current <- vals[j]
+  
+  if(arr[j] == arr[j-1])
+    current <- -1*current
+  
+  current
+}
+
+res <- sapply(3:length(ints), myf, lets, ints)

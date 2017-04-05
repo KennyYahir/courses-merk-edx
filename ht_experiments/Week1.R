a0027f2c8fcfdf9e1bdd49b4e01d70f6261ff071
Kenny Yahir <kenny.puma1030@gmail.com>
2017-04-04 22:47:15 -0500

    More completed tasks

--- ht_experiments/Week1.R
+++ ht_experiments/Week1.R
@@ -11,11 +11,64 @@ head(sampleInfo)
 
 sum(sampleInfo$date == as.Date("2005-06-27"))
 
+
 ## Excercise 2
+
+
+dim(geneAnnotation)  # dimensions
+dim(geneExpression)
+dim(sampleInfo)
 
 head(geneAnnotation)
 head(geneExpression)
 
+
+unique(geneAnnotation$CHR)  # we look at the different chromosomes
+
+library(dplyr)
+
+sum(geneAnnotation$CHR == "chrY", na.rm = T)  # how many genes are on
+                                              # are on chromosome Y?
+
+## Excercise 3
+
+# What is the log expression value of the for gene ARPC1A on the one subject
+# that we measured on 2005-06-10?
+
+
+rownames(geneExpression[1:3, ])
+
+id.sel <- geneAnnotation[!is.na(geneAnnotation$SYMBOL) & geneAnnotation$SYMBOL == "ARPC1A",
+                         "PROBEID"]  # manufacturer ID
+
+subject.id <- sampleInfo[sampleInfo$date == as.Date("2005-06-10"),
+                         "filename"]  # File name
+
+geneExpression[id.sel, subject.id]  # Answer
+
+
+
+## Excercise 4
+
+# Use the function apply to find the median value of each column.
+# What is the median value of these values?
+
+x.medians <- apply(geneExpression, 2, median)
+median(x.medians)
+
+
+
+
+## Excercise 5
+
+GetTTest <- function(e, group){
+  return(t.test(e[group == 1], e[group == 0])$p.value)
+}
+
+g <- factor(sampleInfo$group)  # Encode
+
+# suppose the order of the samples is identical to the encoding
+min(apply(geneExpression, 1, GetTTest, g))  # minimum
 
 ## Time series
 
@@ -79,3 +132,69 @@ myf <- function(j, arr, vals){
 }
 
 res <- sapply(3:length(ints), myf, lets, ints)
+
+
+# Inference in Practice Exercises
+
+## Exercise 1
+
+set.seed(1)
+library(downloader)
+url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename = "femaleControlsPopulation.csv"
+if (!file.exists(filename)) download(url,destfile=filename)
+population = read.csv(filename)
+pvals <- replicate(1000,{
+  control = sample(population[,1],12)
+  treatment = sample(population[,1],12)
+  t.test(treatment,control)$p.val
+})
+head(pvals)
+hist(pvals)
+
+### answer
+sum(pvals < 0.05) / length(pvals)
+
+## Exercise 2
+
+sum(pvals < 0.01) / length(pvals)
+
+
+
+
+## Exercise 3
+
+cases = rnorm(10,30,2)
+controls = rnorm(10,30,2)
+t.test(cases,controls)
+
+set.seed(100)
+
+pvals <- replicate(20, {
+  cases = rnorm(10, 30, 2)
+  controls = rnorm(10, 30, 2)
+  t.test(cases, controls)$p.value
+})
+
+sum(pvals < 0.05)
+
+
+## Exercise 4
+
+set.seed(100)
+
+pvals.rejected <- replicate(1000, {
+  pvals <- replicate(20, {
+    cases = rnorm(10, 30, 2)
+    controls = rnorm(10, 30, 2)
+    t.test(cases, controls)$p.value
+  })
+  
+  sum(pvals < 0.05)
+})
+
+mean(pvals.rejected)
+
+## Exercise 5 (tricky by the "at least once")
+
+sum(pvals.rejected >= 1) / length(pvals.rejected)
